21291b5c9b5172db965285cd5ece7f0fc15a86b2
Rodrigo García S <rod@rodrigogarcia.net>
2012-01-19 11:49:09 -0300

    Initial Commit

---
+++ BundlePoc/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  BundlePoc
+//
+//  Created by Rodrigo Garcia on 1/19/12.
+//  Copyright (c) 2012 Excelsys Soluciones SpA. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
